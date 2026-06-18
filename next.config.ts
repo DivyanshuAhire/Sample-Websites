8df46c6ca0f2674c094e322ddc1b9e6db0c2cee3
DivyanshuAhire <divyanshuahire7@gmail.com>
2026-06-19 01:23:37 +0530

    vercel deployent issue fixxed

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Sample-Websites",
-  assetPrefix: "/Sample-Websites/",
   trailingSlash: true,
 };
 
